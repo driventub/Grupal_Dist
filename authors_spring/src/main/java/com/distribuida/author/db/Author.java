5c8221cfdd662d15330d86432911c1387c3e4d21
david <dfboadaa@uce.edu.ec>
2024-07-17 12:40:59 -0500

    authors_spring modulo

---
+++ authors_spring/src/main/java/com/distribuida/author/db/Author.java
@@ -0,0 +1,47 @@
+package com.distribuida.author.db;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "author")
+//@Data
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+//    private List<Integer> booksId;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
